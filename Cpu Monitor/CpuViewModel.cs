d39a51a72bf6fa1f81aa9ae0a8210cd68eb1a3da
Mehmet Emre Tülek <memretulek@gmail.com>
2024-12-11 20:42:53 +0300

    Add project files.

---
+++ Cpu Monitor/CpuViewModel.cs
@@ -0,0 +1,158 @@
+﻿using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using Widgets.Common;
+
+namespace CPU_Monitor
+{
+    public class CpuViewModel: INotifyPropertyChanged,IDisposable
+    {
+        private readonly Schedule schedule = new();
+        private string scheduleID = "";
+        private readonly CancellationTokenSource cancellationTokenSource = new();
+
+        public struct SettingsStruct
+        {
+            public float TimeLine { get; set; }
+            public string GraphicColor { get; set; }
+        }
+
+        public static SettingsStruct Default => new()
+        {
+            TimeLine = 200,
+            GraphicColor = "#347aeb",
+        };
+
+        public required SettingsStruct Settings = Default;
+
+        private PerformanceCounter? cpuCounter;
+        private AreaSeries? AreaSeries;
+        private int timeCounter;
+
+
+        private PlotModel? _plotModel;
+        public PlotModel? CpuPlotModel 
+        {
+            get { return _plotModel; }
+            set { 
+                _plotModel = value;
+                OnPropertyChanged(nameof(CpuPlotModel));
+            }
+        }
+
+        private string _cpuUsageText = "0";
+        public string CpuUsageText
+        {
+            get { return _cpuUsageText; } 
+            set { 
+                _cpuUsageText = value;
+                OnPropertyChanged(nameof(CpuUsageText));
+            }
+        }
+
+        public async Task Start()
+        {
+            await Task.Run(() =>
+            {
+                cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            }, cancellationTokenSource.Token);
+
+            CreatePlot();
+            UpdateCpuUsage();
+
+            scheduleID = schedule.Secondly(UpdateCpuUsage, 1);
+        }
+
+        private void CreatePlot()
+        {
+            CpuPlotModel = new PlotModel
+            {
+                PlotAreaBorderThickness = new OxyThickness(0),
+                PlotAreaBorderColor = OxyColors.Transparent,
+                Padding = new OxyThickness(0)
+            };
+
+            var xAxis = new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                IsAxisVisible = false,
+                MajorGridlineStyle = LineStyle.None,
+                MinorGridlineStyle = LineStyle.None,
+                MaximumPadding = 0,
+                MinimumPadding = 0                  
+            };
+
+            var yAxis = new LinearAxis
+            {
+                Minimum = 0,
+                Maximum = 100,
+                Position = AxisPosition.Left,
+                IsAxisVisible = false,
+                MajorGridlineStyle = LineStyle.None,
+                MinorGridlineStyle = LineStyle.None,
+                MaximumPadding = 0,
+                MinimumPadding = 0
+            };
+
+            CpuPlotModel.Axes.Add(xAxis);
+            CpuPlotModel.Axes.Add(yAxis);
+
+            AreaSeries = new AreaSeries
+            {
+                LineStyle = LineStyle.Solid,
+                StrokeThickness = 1,
+                Color = OxyColor.Parse(Settings.GraphicColor)
+            };
+
+            CpuPlotModel.Series.Add(AreaSeries);
+        }
+
+        private void UpdateCpuUsage()
+        {
+            if (CpuPlotModel is null || cpuCounter is null || AreaSeries is null) return;
+
+            double cpuUsage = cpuCounter.NextValue();
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                try
+                {
+                    CpuUsageText = $"CPU {cpuUsage:F2}%";
+
+                    AreaSeries.Points.Add(new DataPoint(timeCounter, cpuUsage));
+                    AreaSeries.Points2.Add(new DataPoint(timeCounter, 0));
+                    timeCounter++;
+
+                    if (AreaSeries.Points.Count > Settings.TimeLine)
+                    {
+                        AreaSeries.Points.RemoveAt(0);
+                        AreaSeries.Points2.RemoveAt(0);
+                    }
+
+                    CpuPlotModel.InvalidatePlot(true);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex.Message);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            schedule.Stop(scheduleID);
+            cancellationTokenSource.Cancel();
+            GC.SuppressFinalize(this);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Cpu Monitor/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Widgets.Common;
+
+namespace CPU_Monitor
+{
+    public partial class MainWindow : Window,IWidgetWindow
+    {
+        public readonly static string WidgetName = "Cpu Monitor";
+        public readonly static string SettingsFile = "settings.cpumonitor.json";
+        private readonly Config config = new(SettingsFile);
+
+        public CpuViewModel ViewModel { get; set; }
+        private CpuViewModel.SettingsStruct Settings = CpuViewModel.Default;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadSettings();
+            ViewModel = new()
+            {
+                Settings = Settings
+            };
+            DataContext = ViewModel;
+            _ = ViewModel.Start();
+            Logger.Info($"{WidgetName} is started");
+        }
+
+
+        public void LoadSettings()
+        {
+            try
+            {
+                Settings.GraphicColor = PropertyParser.ToString(config.GetValue("graphic_color"), Settings.GraphicColor);
+                Settings.TimeLine = PropertyParser.ToFloat(config.GetValue("graphic_timeline"), Settings.TimeLine);
+                UsageText.FontSize = PropertyParser.ToFloat(config.GetValue("usage_font_size"));
+                UsageText.Foreground = PropertyParser.ToColorBrush(config.GetValue("usage_foreground"));
+            }
+            catch (Exception)
+            {
+                config.Add("usage_font_size", UsageText.FontSize);
+                config.Add("usage_foreground", UsageText.Foreground);
+                config.Add("graphic_color", Settings.GraphicColor);
+                config.Add("graphic_timeline", Settings.TimeLine);
+                config.Save();
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            ViewModel.Dispose();
+            Logger.Info($"{WidgetName} is closed");
+        }
+
+        public WidgetWindow WidgetWindow()
+        {
+            return new WidgetWindow(this, WidgetDefaultStruct());
+        }
+
+        public static WidgetDefaultStruct WidgetDefaultStruct()
+        {
+            return new WidgetDefaultStruct();
+        }
+    }
+}
